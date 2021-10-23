f8d65b8f7bec0e93abdca5dd4b0b320c576f4ca7
Joaquin Garcia <jgarcia@tpssa.com.ar>
2021-10-23 20:48:05 -0300

    Agregar funcion onClick y comportamiento al boton

--- app/src/main/java/com/example/holamundo/MainActivity.kt
+++ app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.holamundo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onClick(v: View?) {
+        val texto = findViewById<TextView>(R.id.textView)
+        texto.text="¡Hola Kotlin!"
     }
 }
